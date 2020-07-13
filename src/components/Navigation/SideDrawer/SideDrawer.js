54dbe97c1851f9ddce09a0ebd3107fdd45e3cc21
alexx-v <aleksandr1vlk@gmail.com>
2020-07-13 11:36:13 +0300

    SideDrawer functionality

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,17 +4,28 @@ import classes from './SideDrawer.module.css';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxiliary';
 
 function SideDrawer(props) {
+	let attachedClasses = [classes.SideDrawer, classes.Close];
+
+	if (props.open) {
+		attachedClasses = [classes.SideDrawer, classes.Open];
+	}
+
 	return (
-		<div className={classes.SideDrawer}>
-			<div className={classes.Logo}>
-				<Logo />
+		<Aux>
+			<Backdrop show={props.open} clicked={props.closed} />
+			<div className={attachedClasses.join(' ')}>
+				<div className={classes.Logo}>
+					<Logo />
+				</div>
+				<nav>
+					<NavigationItems />
+				</nav>
 			</div>
-			<nav>
-				<NavigationItems />
-			</nav>
-		</div>
+		</Aux>
 	);
 }
 
