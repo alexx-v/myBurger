4dc5bfa5d532d826ff0818a0821b22081ce0de49
alexx-v <aleksandr1vlk@gmail.com>
2020-07-19 13:06:27 +0300

    form elements configured

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -5,37 +5,73 @@ import classes from './ContactData.module.css';
 
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner//Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
 	state = {
-		name: '',
-		email: '',
-		address: {
-			street: '',
-			postalCode: '',
+		orderForm: {
+			name: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Your Name',
+				},
+				value: '',
+			},
+			street: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Street',
+				},
+				value: '',
+			},
+			zipCode: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'ZIP Code',
+				},
+				value: '',
+			},
+			country: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Country',
+				},
+				value: '',
+			},
+			email: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'email',
+					placeholder: 'Your E-Mail',
+				},
+				value: '',
+			},
+			deliveryMethod: {
+				elementType: 'select',
+				elementConfig: {
+					options: [
+						{ value: 'fastest', displayValue: 'Fastest' },
+						{ value: 'cheapest', displayValue: 'Cheapest' },
+					],
+				},
+				value: '',
+			},
 		},
+
 		loading: false,
 	};
 
 	orderHandler = (event) => {
 		event.preventDefault();
 
-		console.log('ContactData ingredients:', this.props.ingredients);
-
 		this.setState({ loading: true });
 		const order = {
 			ingredients: this.props.ingredients,
 			price: this.props.price / 100,
-			customer: {
-				name: 'Max Schwarz',
-				address: {
-					street: 'Teststreet 1',
-					zipCode: '2342342',
-					country: 'Germany',
-				},
-				email: 'test@test.com',
-			},
-			deliveryMethod: 'fastest',
 		};
 		console.log('ContactData ingredients:', order);
 		axios
@@ -47,33 +83,44 @@ class ContactData extends Component {
 			.catch((error) => this.setState({ loading: false }));
 	};
 
+	inputChangedHandler = (event, inputIdentifier) => {
+		// console.log('event.target.value:', event.target.value);
+
+		const updatedOrderForm = {
+			...this.state.orderForm,
+		};
+
+		const updatedFormElement = {
+			...updatedOrderForm[inputIdentifier],
+		};
+
+		updatedFormElement.value = event.target.value;
+		updatedOrderForm[inputIdentifier] = updatedFormElement;
+
+		this.setState({ orderForm: updatedOrderForm });
+	};
+
 	render() {
+		const formElementsArray = [];
+
+		for (let key in this.state.orderForm) {
+			formElementsArray.push({
+				id: key,
+				config: this.state.orderForm[key],
+			});
+		}
+
 		let form = (
 			<form>
-				<input
-					className={classes.Input}
-					type='text'
-					name='name'
-					placeholder='Your Name'
-				/>
-				<input
-					className={classes.Input}
-					type='email'
-					name='email'
-					placeholder='Your Email'
-				/>
-				<input
-					className={classes.Input}
-					type='text'
-					name='street'
-					placeholder='Your Street'
-				/>
-				<input
-					className={classes.Input}
-					type='text'
-					name='postal'
-					placeholder='Your Postal Code'
-				/>
+				{formElementsArray.map((formElement) => (
+					<Input
+						key={formElement.id}
+						elementType={formElement.config.elementType}
+						elementConfig={formElement.config.elementConfig}
+						value={formElement.config.value}
+						changed={(event) => this.inputChangedHandler(event, formElement.id)}
+					/>
+				))}
 				<Button btnType='Success' clicked={this.orderHandler}>
 					ORDER
 				</Button>
