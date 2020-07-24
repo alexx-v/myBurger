9d7741b83925b0f1dc864ccfd0208b28e5225cec
alexx-v <aleksandr1vlk@gmail.com>
2020-07-24 19:15:42 +0300

    minor changes

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,11 +4,13 @@ import { connect } from 'react-redux';
 
 import classes from './ContactData.module.css';
 
+import * as actions from '../../../store/actions/';
+import { checkValidity } from '../../../shared/utility';
+
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner//Spinner';
 import Input from '../../../components/UI/Input/Input';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
-import * as actions from '../../../store/actions/';
 
 class ContactData extends Component {
 	state = {
@@ -118,38 +120,6 @@ class ContactData extends Component {
 		this.props.onOrderBurger(order, this.props.token);
 	};
 
-	checkValidity(value, rules) {
-		let isValid = true;
-
-		if (!rules) {
-			return true;
-		}
-
-		if (rules.required) {
-			isValid = value.trim() !== '' && isValid;
-		}
-
-		if (rules.minLength) {
-			isValid = value.length >= rules.minLength && isValid;
-		}
-
-		if (rules.maxLength) {
-			isValid = value.length <= rules.maxLength && isValid;
-		}
-
-		if (rules.isEmail) {
-			const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-			isValid = pattern.test(value) && isValid;
-		}
-
-		if (rules.isNumeric) {
-			const pattern = /^\d+$/;
-			isValid = pattern.test(value) && isValid;
-		}
-
-		return isValid;
-	}
-
 	inputChangedHandler = (event, inputIdentifier) => {
 		// console.log('event.target.value:', event.target.value);
 
@@ -162,7 +132,7 @@ class ContactData extends Component {
 		};
 
 		updatedFormElement.value = event.target.value;
-		updatedFormElement.valid = this.checkValidity(
+		updatedFormElement.valid = checkValidity(
 			updatedFormElement.value,
 			updatedFormElement.validation
 		);
